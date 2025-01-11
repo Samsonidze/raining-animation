621ad782cc9a60ef11e5b9841d2c528b6c5fa5d0
Samsonidze <119052018+Samsonidze@users.noreply.github.com>
2025-01-11 04:13:08 +0400

    Add files via upload

--- sound.js
+++ sound.js
@@ -1,11 +1,3 @@
-window.addEventListener('load', () => {
-    const audio = document.getElementById('background-music');
-    audio.play().catch(error => {
-      console.log('Autoplay failed. Attempting with user interaction.');
-    });
-
-    document.body.addEventListener('click', () => {
-      audio.muted = false;
-      audio.play();
-    });
-  });
+window.addEventListener('load', () =>{
+    document.getElementById("song").play()
+})
