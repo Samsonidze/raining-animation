625a35cdfb18912782ab1b59b16138b873260694
Samsonidze <119052018+Samsonidze@users.noreply.github.com>
2025-01-11 04:22:38 +0400

    Add files via upload

--- sound.js
+++ sound.js
@@ -1,9 +1,6 @@
-window.addEventListener('load', () => {
-    // Wait for user interaction before playing the audio
-    document.body.addEventListener('click', () => {
-      const audio = document.getElementById("song");
-      audio.play().catch(error => {
-        console.log("Autoplay failed: ", error);
-      });
+document.getElementById("playButton").addEventListener("click", function() {
+    const audio = document.getElementById("song");
+    audio.play().catch(error => {
+      console.log("Autoplay failed: ", error);
     });
   });
